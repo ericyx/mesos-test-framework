7a1e866fc8717bbefffed0b30f9657b4b60d3268
ericyx <eric_yangxin@163.com>
2015-05-26 14:47:43 -0400

    cmake success

---
+++ build/src/CMakeFiles/mesos_test_executor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mesos_test_executor.dir/test_executor.cpp.o"
+  "mesos_test_executor.pdb"
+  "mesos_test_executor"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mesos_test_executor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
